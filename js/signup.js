35a72a4acf03b55efb14fe9f4e945e804ec85982
Juliana Mota <julianamota89@gmail.com>
2019-05-28 15:24:24 +0200

    add js for lable and style form

--- js/signup.js
+++ js/signup.js
@@ -4,12 +4,24 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   document.querySelector(".btn-email").addEventListener("click", signupCliked);
+  const inputs = document.querySelectorAll(".inputLabel");
+  console.log(inputs);
+  inputs.forEach(oneInput => oneInput.addEventListener("click", animatelable));
 }
 
-function signupCliked(e) {
+function signupCliked() {
   const form = document.querySelector("#acount-form");
   console.log(form);
   form.classList.add("animateform");
   const acountChoice = document.querySelector("#acount-choose");
-  acountChoice.classList.add("hide");
+  acountChoice.classList.add("hideChosse");
+}
+
+function animatelable(e) {
+  console.log(e.target.id);
+  console.log(e.toElement.nextElementSibling.htmlFor);
+
+  if (e.target.id === e.toElement.nextElementSibling.htmlFor) {
+    e.toElement.nextElementSibling.classList.add("move-lable");
+  }
 }
