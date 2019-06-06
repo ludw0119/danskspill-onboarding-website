b34329fe6870b6364941960d6356c3d36256f665
Juliana Mota <julianamota89@gmail.com>
2019-06-06 15:03:51 +0200

    take cursor image

---
+++ dist/js/list.js
@@ -0,0 +1,278 @@
+let modal = document.querySelector(".modal");
+let modalClose = document.querySelector(".modalClose");
+let deactivatedList = [];
+let arrayOfUsers = [];
+
+//prototype object
+const userObject = {
+  Photo: "-user photo-",
+  Username: "-user name-",
+  Password: "-user password-",
+  Wins: "-wins-",
+  Looses: "-looses-",
+  Rating: "-rating-",
+  Fullname: "-fullname-",
+  Email: "-email-",
+  Telephone: "-telephone-",
+  Address: "-address-",
+  Id: "-id-"
+};
+
+/*------------------------------------------------------------------------making object---------------------------------------------------------------------*/
+
+function getJSON() {
+  fetch("https://dantoto-eb44.restdb.io/rest/dantoto-users", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5ce6c77b780a473c8df5cb6d",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(makeObject);
+}
+getJSON();
+
+function makeObject(usersList) {
+  //alert("make obj");
+  usersList.forEach(user => {
+    const newUserObject = Object.create(userObject);
+    newUserObject.photo = "https://dantoto-eb44.restdb.io/media/" + user.Photo;
+    newUserObject.username = user.Username;
+    newUserObject.password = user.Password;
+    newUserObject.wins = user.Wins;
+    newUserObject.looses = user.Looses;
+    newUserObject.rating = user.Rating;
+    newUserObject.fullname = user.Fullname;
+    newUserObject.email = user.Email;
+    newUserObject.telephone = user.Telephone;
+    newUserObject.address = user.Address;
+    newUserObject.id = user._id;
+    arrayOfUsers.push(newUserObject);
+    //console.log(user.Username);
+    //console.log(newUserObject.username);
+  });
+  displayUsers(arrayOfUsers);
+}
+
+/*---------------------------------------------------------------displaying table 1 content - with object-----------------------------------------------------------------*/
+
+function displayUsers(arrayOfUsers) {
+  document.querySelectorAll(".tableRow").forEach(item => {
+    item.remove(); //the rows need to deleted before displaying users, because otherwise they are duplicated if the function is called 2 or more times in the code
+  });
+  arrayOfUsers.forEach(user => {
+    const template1 = document.querySelector("#template1").content;
+    const template2 = document.querySelector("#template2").content;
+    const clone = template1.cloneNode(true);
+    const clone2 = template2.cloneNode(true);
+    //console.log(clone);
+    console.log(user.photo);
+
+    if (
+      user.photo === "https://dantoto-eb44.restdb.io/media/" ||
+      user.photo === "https://dantoto-eb44.restdb.io/media/undefined"
+    ) {
+      clone.querySelector(".photoTable").src = "./images/noPhoto.png";
+    } else {
+      clone.querySelector(".photoTable").src = user.photo;
+    }
+    clone.querySelector(".name").textContent = user.username;
+    clone.querySelector(".wins").textContent = user.wins;
+    clone.querySelector(".looses").textContent = user.looses;
+    clone.querySelector(".rating").textContent = user.rating;
+    let removeButtonId = "removeButton" + user.id;
+    clone.querySelector(".removeButton").id = removeButtonId;
+    removeButtonObject = clone.querySelector(".removeButton");
+    removeButtonObject.addEventListener("click", e => {
+      showWarning(user.id);
+    });
+
+    //warning modal cloned
+    clone2.querySelector(".warningWrapper").id = "warningWrapper" + user.id;
+
+    //console.log(user.id);
+    let rows = clone.querySelectorAll(".row");
+    rows.forEach(row => {
+      row.addEventListener("click", e => {
+        showModal(user.id);
+        document.querySelector("#deactivateButton").style.display = "block";
+      });
+    });
+
+    //warning modal closing
+    let warningCloses = clone2.querySelectorAll(".warningClose");
+    warningCloses.forEach(warningClose => {
+      warningClose.addEventListener("click", e => {
+        hideWarning(user.id);
+      });
+    });
+
+    //warning modal delete buttons
+    let deleteButtons = clone2.querySelectorAll(".deleteButton");
+    deleteButtons.forEach(delButton => {
+      delButton.addEventListener("click", e => {
+        deleteUser(user.id);
+      });
+    });
+
+    document.querySelector("#table1").appendChild(clone);
+    document.querySelector("#parent2").appendChild(clone2);
+  });
+}
+
+function showWarning(id) {
+  let devID = "#warningWrapper" + id;
+  document.querySelector(devID).style.display = "block";
+  //alert("warning alert");
+}
+
+function hideWarning(id) {
+  let devID = "#warningWrapper" + id;
+  document.querySelector(devID).style.display = "none";
+}
+
+/*---------------------------------------------------------------displaying table 2 content - with object-----------------------------------------------------------------*/
+
+function displayDeactivated(deactivatedList) {
+  deactivatedList.forEach(user => {
+    const template = document.querySelector("#template1").content;
+    const clone = template.cloneNode(true);
+    //console.log(clone);
+
+    if (user.photo != null) {
+      if (
+        user.photo === "https://dantoto-eb44.restdb.io/media/" ||
+        user.photo === "https://dantoto-eb44.restdb.io/media/undefined"
+      ) {
+        clone.querySelector(".photoTable").src = "../images/noPhoto.png";
+      } else {
+        clone.querySelector(".photoTable").src = user.photo;
+      }
+    }
+
+    clone.querySelector(".name").textContent = user.username;
+    clone.querySelector(".wins").textContent = user.wins;
+    clone.querySelector(".looses").textContent = user.looses;
+    clone.querySelector(".rating").textContent = user.rating;
+    //clone.querySelector(".removeButton").id = "removeButton" + user.id;
+    let removeButtonId = "removeButton" + user.id;
+    clone.querySelector(".removeButton").id = removeButtonId;
+    removeButtonObject = clone.querySelector(".removeButton");
+    removeButtonObject.addEventListener("click", e => {
+      showWarning(user.id);
+      //console.log(user.username);
+    });
+    let rows = clone.querySelectorAll(".row");
+    rows.forEach(row => {
+      row.addEventListener("click", e => {
+        showModal(user.id);
+        document.querySelector("#deactivateButton").style.display = "none";
+      });
+    });
+    document.querySelector("#table2").appendChild(clone);
+    document.querySelector("#table2Wrapper").classList.remove("invisible");
+  });
+  if (deactivatedList.length == 0) {
+    document.querySelector("#table2Wrapper").classList.add("invisible");
+  }
+}
+
+/*---------------------------------------------------------------deleting from database-----------------------------------------------------------------*/
+
+function deleteUser(id) {
+  //deleting from the database
+
+  fetch("https://dantoto-eb44.restdb.io/rest/dantoto-users/" + id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5ce6c77b780a473c8df5cb6d",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {});
+  //deleting from the object
+  let arrayId = findById(id, arrayOfUsers);
+  if (arrayId < 0) {
+    //removing from deactivated
+    arrayId = findById(id, deactivatedList);
+    deactivatedList.splice(arrayId, 1);
+  } else {
+    //removing from users
+    arrayOfUsers.splice(arrayId, 1);
+  }
+
+  //updating the table view
+  hideWarning(id);
+  displayUsers(arrayOfUsers);
+  displayDeactivated(deactivatedList);
+}
+
+function findById(id, arrayToCheck) {
+  //as this function checks both arrays,array to check has to be passed
+  return arrayToCheck.findIndex(obj => obj.id === id); //the function finds ID of a user in the object (ID in function "deleteTask" argument is an ID number from the database (different than in object)
+}
+
+/*---------------------------------------------------------------------modal---------------------------------------------------------------------------*/
+function showModal(id) {
+  //console.log(id);
+
+  fetch("https://dantoto-eb44.restdb.io/rest/dantoto-users/" + id, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5ce6c77b780a473c8df5cb6d",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      //console.log(data);
+      modal.style.display = "block";
+      let photo = document.querySelector(".photoModal");
+      let username = document.querySelector("#Username");
+      let fullname = document.querySelector("#Fullname");
+      let email = document.querySelector("#Email");
+      let telephone = document.querySelector("#Telephone");
+      let address = document.querySelector("#Address");
+      let button = document.querySelector("#deactivateButton");
+      console.log(data.Photo);
+
+      if (data.Photo === undefined || data.Photo.length === 0) {
+        photo.src = "../images/noPhoto.png";
+      } else {
+        photo.src = "https://dantoto-eb44.restdb.io/media/" + data.Photo;
+      }
+
+      //photo.src = "https://dantoto-eb44.restdb.io/media/" + data.Photo;
+      username.textContent = data.Username;
+      fullname.textContent = data.Fullname;
+      email.textContent = data.Email;
+      telephone.textContent = data.Telephone;
+      address.textContent = data.Address;
+
+      modalClose.onclick = function() {
+        hideModal();
+      };
+      button.onclick = function() {
+        deactivateUser(id);
+      };
+    });
+}
+
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function deactivateUser(id) {
+  let rowNo = findById(id, arrayOfUsers);
+  deactivatedList.push(arrayOfUsers[rowNo]);
+  let obj = arrayOfUsers.splice(rowNo, 1); //the variable has a value of a number so it can be used as index in splice
+  displayUsers(arrayOfUsers);
+  hideModal();
+  displayDeactivated(deactivatedList);
+  hideModal();
+}
---
+++ dist/js/script.js
@@ -0,0 +1,353 @@
+window.addEventListener("load", loadSVG);
+
+//show graphic - load SVG
+
+function loadSVG() {
+  fetch("prototypesave1_game 1.svg")
+    .then(res => res.text())
+    .then(svgdata => {
+      document
+        .querySelector("#gameBox")
+        .insertAdjacentHTML("afterbegin", svgdata);
+
+      document
+        .querySelector("#playButton")
+        .addEventListener("mouseover", () => {
+          TweenLite.to("#playButton", 0.2, {
+            scale: 0.96,
+            transformOrigin: "50% 50%"
+          });
+        });
+      document.querySelector("#playButton").addEventListener("mouseout", () => {
+        TweenLite.to("#playButton", 0.2, {
+          scale: 1,
+          transformOrigin: "50% 50%"
+        });
+      });
+      animationFallingBoxes();
+      animateListOfHorses();
+
+      console.log(document.querySelector("#pointsText").textContent);
+      if (document.querySelector("#pointsText").textContent == 0) {
+        document.querySelector("#youWonPlaceholder").innerHTML =
+          "Are you sure you don't want to play?";
+        document.querySelector("#savePoint").innerHTML =
+          "If you play you can win points for the real betting";
+      }
+    });
+}
+
+// --------------------------------------- animate list of horses ----------------------------------
+
+function animationFallingBoxes() {
+
+  document.querySelector("#playButton").addEventListener("click", () => {
+    document.querySelector("#youWonPlaceholder").innerHTML =
+      "YOU WON <span id='pointsPlaceholder'>30</span> POINTS";
+
+    document.querySelector("#FIRST").classList.add("hide");
+    TweenLite.to("#playButton", 1, {
+      scale: 0.5
+    });
+
+    let whooshSound = document.querySelectorAll(
+      "#whooshSound0, #whooshSound1, #whooshSound2, #whooshSound3"
+    );
+    let boxesWithHorses = document.querySelectorAll(
+      "#horse1, #horse2, #horse3, #horse4"
+    );
+
+    for (let i = 0; i < boxesWithHorses.length; i++) {
+      console.log(i);
+
+      TweenLite.from(boxesWithHorses[i], 1, {
+        delay: i / 2,
+        y: "-=519",
+        ease: Elastic.easeOut.config(0.5, 0.3),
+        onStart: function() {
+          console.log("play " + i);
+          whooshSound[i].play();
+          //document.querySelector("#whooshSound0").play();
+        }
+      });
+    }
+    document.querySelector("#SECOND").classList.remove("hide");
+    document
+      .querySelectorAll("#horse1, #horse2, #horse3, #horse4")
+      .forEach(element => {
+        element.addEventListener("click", event => {
+          clickToBet(event);
+          document.querySelector("#SECOND").classList.add("hide");
+          document.querySelector("#THIRD").classList.remove("hide");
+
+          TweenMax.to("#lets-race", 1.4, {
+            scale: 5,
+            opacity: 0,
+            transformOrigin: "50% 50%"
+          });
+
+          // document.querySelector("#lets-race").addEventListener("click", () => {
+          //   document.querySelector("#THIRD").classList.add("hide");
+          //   document.querySelector(".cls-4-1").classList.remove("hide");
+          //  });
+        });
+      });
+  });
+}
+
+function animateListOfHorses() {
+  let allBoxes = document.querySelectorAll(".orangeBox");
+
+  let allBoxes_a = document.querySelectorAll(".cls-2-4a");
+  let textGroup = document.querySelectorAll(".text-group");
+  let allImageHorses = document.querySelectorAll(
+    "#horseImage1, #horseImage2, #horseImage3, #horseImage4"
+  );
+
+  console.log(allBoxes);
+  for (let i = 0; i < allBoxes.length; i++) {
+    Array.from(allBoxes[i].querySelectorAll("text, path")).forEach(item => {
+      item.addEventListener("mouseenter", () => {
+        event.preventDefault();
+        console.log(event.target);
+        TweenMax.to(allBoxes_a[i], 0.4, {
+          scaleY: -1.2,
+          y: "1"
+        });
+        TweenMax.to(allImageHorses[i], 0.4, {
+          scaleY: 0,
+          transformOrigin: "100% 100%"
+        });
+        TweenMax.to(textGroup[i], 0.4, {
+          y: "-70"
+        });
+      });
+    });
+  }
+  for (let i = 0; i < allBoxes.length; i++) {
+    Array.from(allBoxes[i].querySelectorAll("path, text")).forEach(item => {
+      item.addEventListener("mouseleave", () => {
+        event.preventDefault();
+        console.log(event.target);
+        TweenMax.to(allBoxes_a[i], 0.4, {
+          scaleY: 0
+        });
+        TweenMax.to(allImageHorses[i], 0.4, {
+          scaleY: 0.33,
+          transformOrigin: "100% 100%"
+        });
+        TweenMax.to(textGroup[i], 0.4, {
+          y: "0"
+        });
+      });
+    });
+  }
+}
+
+// ---------------------------------------------- race -----------------------------------------
+
+let i;
+let time;
+let scores = [];
+let horses = [];
+let chosenHorse;
+let position;
+
+// bet on a horse
+
+function clickToBet(event) {
+  // Create a condition to add event listener only to the elements that  contains an attribute “data – horse”
+  if (event.target.parentElement.parentElement.dataset.horse) {
+    // Set the value a variable “chosenHorse” with the value of the “data – horse” attribute
+    chosenHorse = event.target.parentElement.parentElement.dataset.horse;
+    horseRace();
+
+    console.log(
+      chosenHorse,
+      document.querySelector("#race [data-horse='" + chosenHorse + "']")
+    );
+  }
+}
+
+let randomVal = Math.random(); // Variable used to create a random easing for the moving horses
+
+function horseRace() {
+  let raceSound = document.querySelector("#raceSound");
+
+  let animateArrayOfHorses = document.querySelectorAll(
+    "#white-horse, #brown-white-horse, #black-horse, #brown-horse"
+  ); // the array contains all the horses from the scene with race, that will be animate with for loop function
+
+  document.querySelector(
+    "#race [data-horse='" + chosenHorse + "']"
+  ).style.opacity = "0.7";
+
+  for (let i = 0; i < animateArrayOfHorses.length; i++) {
+    raceSound.play();
+    randomVal = Math.random();
+
+    CustomEase.create(
+      "run" + [i],
+      "M0,0 C0.216,0.088 0.436,0.168 0.52,0.298 0.627,0.464 0.621,0.492 " +
+        randomVal +
+        ",0.664 0.804,0.804 0.856,0.81 1,1"
+    );
+    let roundRandom = Math.random() * 10;
+    time = roundRandom + 10;
+    let element = animateArrayOfHorses[i];
+
+    let tl = new TimelineMax({ repeat: -1, repeatDelay: 0 });
+    tl.from(element, roundRandom / 10 + 0.2, {
+      ease: Power2.easeIn,
+      rotation: -10,
+      transformOrigin: "50% 50%"
+    });
+    tl.to(element, roundRandom / 10 + 0.2, {
+      ease: Power2.easeOut,
+      rotation: 10,
+      transformOrigin: "50% 50%"
+    });
+    tl.from(element, roundRandom / 10 + 0.2, {
+      ease: Power2.easeIn,
+      rotation: 10,
+      transformOrigin: "50% 50%"
+    });
+    tl.to(element, roundRandom / 10 + 0.2, {
+      ease: Power2.easeOut,
+      rotation: -10,
+      transformOrigin: "50% 50%"
+    });
+
+    TweenLite.to(element, time, {
+      ease: "run" + [i],
+      x: 850,
+
+      onComplete: function() {
+        console.log(i, Math.round(time * 100) / 100);
+        scores.push(Math.round(time * 100) / 100);
+        horses.push(element.dataset.name);
+        time = Math.random() * 10 + 10;
+
+        scores.sort(function(a, b) {
+          return a - b;
+        });
+        console.log(horses);
+        console.log(horses.length);
+        if (horses.length === 4) {
+          showScores();
+          position = horses.indexOf(chosenHorse);
+          console.log(position);
+          document.querySelector("#THIRD").classList.add("hide");
+          document.querySelector(".cls-4-1").classList.remove("hide");
+          let scorseBoxes = Array.from(document.querySelectorAll(".scorseBox"));
+          scorseBoxes[position].style.opacity = "0.8";
+
+          document
+            .querySelector(".nextButton")
+            .addEventListener("click", showPoints);
+          document
+            .querySelector("#playAgainButton")
+            .addEventListener("click", () => {
+              document.querySelector(".cls-4-1").classList.add("hide");
+              document.querySelector("#FIRST").classList.remove("hide");
+              document.location.reload();
+            });
+        }
+      }
+    });
+  }
+}
+
+// ------------------------------------------- show scores -------------------------------------
+
+function showScores() {
+  let horseNames = document.querySelectorAll(".horseName");
+  let horsesTimes = document.querySelectorAll(".horseTime");
+  console.log(horseNames.length);
+  for (let i = 0; i < horseNames.length; i++) {
+    horseNames[i].textContent = horses[i];
+    horsesTimes[i].textContent = scores[i];
+  }
+}
+
+function showPoints() {
+  console.log(chosenHorse);
+
+  document
+    .querySelectorAll(
+      ".FIFTHcls-9a, .FIFTH2cls-24a, .FIFTH2cls-32a, .FIFTH2cls-39a"
+    )
+    .forEach(horse => {
+      if (horse.dataset.horse === chosenHorse) {
+        horse.classList.remove("hide");
+      }
+      TweenMax.from(horse, 3, {
+        opacity: 0
+      });
+    });
+
+  document
+    .querySelectorAll(".nextButton, #playAgainButton, .scoresBox")
+    .forEach(e => {
+      e.classList.add("hide");
+    });
+  document.querySelector("#FIFTH").classList.remove("hide");
+
+  let horseFeet = document.querySelectorAll(".FIFTHcls-4a");
+  console.log(position);
+  document.querySelector("#pointsText").textContent =
+    50 - position * 10 + " POINTS";
+
+  document.querySelector(
+    "#pointsPlaceholder"
+  ).textContent = document.querySelector("#pointsText").textContent;
+
+  let bellSound = document.querySelectorAll(
+    "#Bell1, #Bell2, #Bell3, #Bell4, #Bell5"
+  );
+
+  for (let i = 0; i < 5 - position - 1 + 1; i++) {
+    TweenMax.to(horseFeet[i], 0.5, {
+      delay: i,
+      fill: "#f7931e",
+      onStart: function() {
+        bellSound[i].play();
+      }
+    });
+  }
+  TweenMax.from("#pointsText", 0.5, {
+    delay: 5 - position - 1 + 1,
+    scale: 5,
+    transformOrigin: "50% 50%",
+    opacity: 0,
+    onStart: function() {
+      document.querySelector("#sucess").play();
+    },
+    ease: Bounce.easeOut
+  });
+
+  let scoresParents = Array.from(document.querySelectorAll(".scoresParent"));
+  let scorseBoxes = Array.from(document.querySelectorAll(".scorseBox"));
+  // box animatiom
+  TweenMax.to(scorseBoxes[position], 0.5, {
+    attr: { y: 258 }
+  });
+  // text animation
+  console.log(scorseBoxes[position].parentNode.childNodes[2]);
+  TweenMax.to(scoresParents[position].querySelector(".cls-4-6"), 0.5, {
+    attr: { transform: "translate(303.23 298.35)" }
+  });
+  TweenMax.to(scoresParents[position].querySelector(".cls-4-7"), 0.5, {
+    attr: { transform: "translate(798.01 300.63)" }
+  });
+  TweenMax.to(scoresParents[position].querySelector(".cls-4-8"), 0.5, {
+    attr: { transform: "translate(195.33 299.36)" }
+  });
+  // remove all postion but one
+  console.log(position);
+  for (let i = 0; i < scoresParents.length; i++) {
+    if (i != position) {
+      scoresParents[i].classList.add("hide");
+    }
+  }
+}
